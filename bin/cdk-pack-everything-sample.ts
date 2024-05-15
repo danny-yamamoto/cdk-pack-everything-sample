e1a56ae7b6736e21410dc556103a2cbec6aafbae
daisuke yamamoto <yamamoto.daisuke.1012@gmail.com>
2024-05-15 01:24:46 +0000

    Run Prettier format

--- bin/cdk-pack-everything-sample.ts
+++ bin/cdk-pack-everything-sample.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkPackEverythingSampleStack } from '../lib/stack/cdk-pack-everything-sample-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { CdkPackEverythingSampleStack } from "../lib/stack/cdk-pack-everything-sample-stack";
 import { devParameter, AppParameter } from "./parameter";
 
 const app = new cdk.App();
@@ -12,23 +12,27 @@ const envKey = app.node.tryGetContext(argContext);
 console.log(envKey);
 const parameters = [devParameter];
 const appParameter: AppParameter = parameters.filter(
-  (obj: AppParameter) => obj.envName === envKey
+  (obj: AppParameter) => obj.envName === envKey,
 )[0];
 console.log(appParameter);
 
-new CdkPackEverythingSampleStack(app, `${appParameter.envName}CdkPackEverythingSampleStack`, {
-  ...appParameter,
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+new CdkPackEverythingSampleStack(
+  app,
+  `${appParameter.envName}CdkPackEverythingSampleStack`,
+  {
+    ...appParameter,
+    /* If you don't specify 'env', this stack will be environment-agnostic.
+     * Account/Region-dependent features and context lookups will not work,
+     * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+    /* Uncomment the next line to specialize this stack for the AWS Account
+     * and Region that are implied by the current CLI configuration. */
+    // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+    /* Uncomment the next line if you know exactly what Account and Region you
+     * want to deploy the stack to. */
+    // env: { account: '123456789012', region: 'us-east-1' },
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+    /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  },
+);
--- lib/stack/cdk-pack-everything-sample-stack.ts
+++ lib/stack/cdk-pack-everything-sample-stack.ts
@@ -1,16 +1,18 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-//import { aws_s3 as s3 } from 'aws-cdk-lib';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import { VpcConstruct } from '../construct/vpc';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { VpcConstruct } from "../construct/vpc";
 
 interface CdkPackEverythingSampleStackProps extends cdk.StackProps {
   envName: string;
 }
 
 export class CdkPackEverythingSampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: CdkPackEverythingSampleStackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: CdkPackEverythingSampleStackProps,
+  ) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
@@ -19,15 +21,10 @@ export class CdkPackEverythingSampleStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkPackEverythingSampleQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
-    /*
-    const vpc = new ec2.Vpc(this, 'SampleVpc', {
-      ipAddresses: ec2.IpAddresses.cidr('192.168.0.0/19'),
-      vpcName: 'cdk-sample-vpc'
-    });
-    */
+
     new VpcConstruct(this, "VpcConstruct", {
       vpcName: "test",
-      cidr: "192.168.224.0/19"
+      cidr: "192.168.224.0/19",
     });
   }
 }
